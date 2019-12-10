409c843e7310cae9a6e1bb209b0d8343f9d5d33f
Jaakko Malkki <Jaakko.Malkki@hsl.fi>
2019-12-10 15:13:40 +0200

    Add support for creating ADDED trips

--- src/main/java/fi/hsl/transitdata/tripupdate/gtfsrt/GtfsRtFactory.java
+++ src/main/java/fi/hsl/transitdata/tripupdate/gtfsrt/GtfsRtFactory.java
@@ -81,7 +81,9 @@ public class GtfsRtFactory {
                 .setDirectionId(direction)
                 .setStartDate(estimate.getTripInfo().getOperatingDay()) // Local date as String
                 .setStartTime(estimate.getTripInfo().getStartTime()) // Local time as String
-                .setScheduleRelationship(GtfsRealtime.TripDescriptor.ScheduleRelationship.SCHEDULED)
+                .setScheduleRelationship((estimate.getTripInfo().hasScheduled() && !estimate.getTripInfo().getScheduled()) ?
+                        GtfsRealtime.TripDescriptor.ScheduleRelationship.ADDED :
+                        GtfsRealtime.TripDescriptor.ScheduleRelationship.SCHEDULED)
                 .build();
 
         GtfsRealtime.TripUpdate.Builder tripUpdateBuilder = GtfsRealtime.TripUpdate.newBuilder()
