176207a8507e5e6b4c2df6ee4c02f6635f1a41b9
Joose Helle <joose.helle@gmail.com>
2020-02-24 13:04:25 +0200

    Add comments

--- src/main/java/fi/hsl/transitdata/tripupdate/processing/TripUpdateProcessor.java
+++ src/main/java/fi/hsl/transitdata/tripupdate/processing/TripUpdateProcessor.java
@@ -179,6 +179,8 @@ public class TripUpdateProcessor {
             // We need to clean up the "raw data" StopTimeUpdates for any inconsistencies
             List<StopTimeUpdate> validated = GtfsRtValidator.cleanStopTimeUpdates(stopTimeUpdates, null);
             if (validated.isEmpty()) {
+                // This is probably cancellation of cancellation (CANCELED -> SCHEDULED/ADDED) as no stop time updates were available
+                // Gtfs-rt standard requires SCHEDULED (OR ADDED) trip update to contain at least one stop time update, thus let's add one
                 GtfsRealtime.TripUpdate.StopTimeUpdate.Builder stopTimeUpdateBuilder = null;
                 stopTimeUpdateBuilder.setStopSequence(1);
                 stopTimeUpdateBuilder.setScheduleRelationship(StopTimeUpdate.ScheduleRelationship.NO_DATA);
