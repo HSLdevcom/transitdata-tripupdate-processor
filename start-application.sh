8cfd7b9de4923d17e78bd2f338ea9a6967a05075
Aki Väliaho <aki.valiaho@hsl.fi>
2020-03-18 19:54:26 +0200

    Fix truthiness error in bash script

--- start-application.sh
+++ start-application.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if [[ -z "${DEBUG_ENABLED}" ]]; then
+if [[ "${DEBUG_ENABLED}" = true ]]; then
   java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005 -Xms256m -Xmx4096m -jar /usr/app/transitdata-tripupdate-processor.jar
 else
-  java -Xms256m -Xmx4096m -jar /usr/app/transitdata-tripupdate-processor.jar
+  java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005 -Xms256m -Xmx4096m -jar /usr/app/transitdata-tripupdate-processor.jar
 fi
