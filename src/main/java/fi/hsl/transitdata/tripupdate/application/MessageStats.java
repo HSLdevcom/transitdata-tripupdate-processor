4cc15cbbae6e6129ed688692acb4a11af75aec62
Jaakko Malkki <Jaakko.Malkki@hsl.fi>
2022-08-15 16:10:07 +0300

    Log reason why trip update was invalid

--- src/main/java/fi/hsl/transitdata/tripupdate/application/MessageStats.java
+++ src/main/java/fi/hsl/transitdata/tripupdate/application/MessageStats.java
@@ -3,12 +3,19 @@ package fi.hsl.transitdata.tripupdate.application;
 
 import org.slf4j.Logger;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 public class MessageStats {
     private long startTime = System.nanoTime();
 
     private int messagesReceived = 0;
     private int messagesSent = 0;
     private int invalidTripUpdates = 0;
+
+    private final Map<String, Integer> invalidTripUpdateReasons = new HashMap<>();
 
     public int getMessagesReceived() {
         return messagesReceived;
@@ -30,8 +37,13 @@ public class MessageStats {
         return invalidTripUpdates;
     }
 
-    public void incrementInvalidTripUpdates() {
+    public void incrementInvalidTripUpdates(final String validator) {
+
         invalidTripUpdates++;
+
+        if (validator != null) {
+            invalidTripUpdateReasons.compute(validator, (key, prev) -> (prev != null ? prev : 0) + 1);
+        }
     }
 
     public long getStartTime() {
@@ -44,9 +56,12 @@ public class MessageStats {
 
     public void reset() {
         startTime = System.nanoTime();
+
         messagesReceived = 0;
         messagesSent = 0;
         invalidTripUpdates = 0;
+
+        invalidTripUpdateReasons.clear();
     }
 
     public void logAndReset(Logger logger) {
@@ -56,10 +71,12 @@ public class MessageStats {
 
     @Override
     public String toString() {
+        final String reasonsText = invalidTripUpdateReasons.entrySet().stream().map(entry -> entry.getKey() + ": " + entry.getValue()).collect(Collectors.joining(", "));
+
         return "Message stats:\n"+
                 "\tStart time: " + getDurationSecs() + " seconds ago\n" +
                 "\tMessages received: " + messagesReceived + "\n" +
                 "\tMessages sent: " + messagesSent + "\n" +
-                "\tInvalid trip updates: " + invalidTripUpdates;
+                "\tInvalid trip updates: " + invalidTripUpdates + "(" + reasonsText + ")";
     }
 }
